be4e44fc6cc1965a618fa8e75ddfc6eedeab1505
Aman-Agnihotri36 <agnihotriaman36@gmail.com>
2025-05-14 22:39:28 +0530

    back_change

--- components/prediction.tsx
+++ components/prediction.tsx
@@ -8,7 +8,7 @@ import { Card, CardContent } from "@/components/ui/card"
 import { Slider } from "@/components/ui/slider"
 import { Progress } from "@/components/ui/progress"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import Sli from "./slider"
+
 
 interface PredictionFormProps {
     // eslint-disable-next-line  @typescript-eslint/no-explicit-any
