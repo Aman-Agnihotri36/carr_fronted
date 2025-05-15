326b41592601324313a1ed980d0b874eaf86df55
Aman-Agnihotri36 <agnihotriaman36@gmail.com>
2025-05-15 21:15:26 +0530

    done

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,7 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { Github, Linkedin } from "lucide-react"
-import Link from "next/link"
+
 
 export default function Footer() {
     return (
